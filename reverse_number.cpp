29bd3aeda4bcd697a7c03e848563ae626a34bb40
Tushar Garg <61476389+tushar152@users.noreply.github.com>
2021-10-12 12:04:02 +0530

    Update reverse_number.cpp

--- reverse_number.cpp
+++ reverse_number.cpp
@@ -1,16 +1,14 @@
 #include<bits/c++.h>
-int main()
-{
+int main() {
   cout<<"Enter the number to  be reversed"<<endl;
   int no,rem,rev;
   cin>>no;
-  while(no>0)
-  {
+  while(no>0) {
     rem=no%10;
     rev=rev*10+rem;
     no=no/10;
-    
   }
+  
   cout<<"The reverse of number is"<< rev<<endl;
   return 0;
 }
